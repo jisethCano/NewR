b268687100ff110c6bd37e4214b13123c315f88c
jisethCano <mjcano501@soy.sena.edu.co>
2023-03-23 03:59:23 -0500

    agregar new

---
+++ src/main/java/com/trafic/Web/WebNewApplication.java
@@ -0,0 +1,13 @@
+package com.trafic.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebNewApplication.class, args);
+	}
+
+}
